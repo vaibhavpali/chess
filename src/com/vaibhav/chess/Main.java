5b5a171ea608c4277746e3049bc04d1a567ad1be
vaibhav paliwal <15090114+vaibhavpali@users.noreply.github.com>
2021-04-13 03:47:59 +0530

    Piece class is initialized using Factory

--- src/com/vaibhav/chess/Main.java
+++ src/com/vaibhav/chess/Main.java
@@ -1,5 +1,50 @@
 package com.vaibhav.chess;
 
-public class Main {
+import com.vaibhav.chess.exception.GameException;
+import com.vaibhav.chess.game.Board;
+import com.vaibhav.chess.game.Cell;
+import com.vaibhav.chess.game.CellDataManagerImpl;
+import com.vaibhav.chess.piece.Piece;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args == null || args.length != 2 || isEmpty(args[0]) || isEmpty(args[1])) {
+            throw new IllegalArgumentException("INVALID_INPUT");
+        }
+        String pieceName = args[0];
+        String identifier = args[1];
+
+        Board chessBoard = new Board();
+        chessBoard.initializeBoard();
+
+        ICellDataManager cellDataManager = new CellDataManagerImpl(chessBoard.getCells());
+
+        Piece piece = PieceFactory.getPiece(pieceName, cellDataManager);
+        Cell cell = cellDataManager.getCellById(identifier);
+
+        try {
+            if (piece == null) {
+                throw new GameException("INVALID_PIECE_NAME", "Please enter valid Piece name, like: " +
+                        "KING, QUEEN, HORSE, BISHOP, ROOK, PAWN");
+            }
+            if (cell == null) {
+                throw new GameException("INVALID_IDENTIFIER", identifier + " not found, please enter valid input");
+            }
+
+            List<Cell> possibleMoves = piece.possibleMoves(cell);
+            List<String> listOfMoves = cellDataManager.getIdentifiers(possibleMoves);
+
+            System.out.println(Arrays.toString(listOfMoves.toArray()) + " :: Count = " + listOfMoves.size());
+        } catch (GameException e) {
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+
+    private static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
 }
--- src/com/vaibhav/chess/PieceFactory.java
+++ src/com/vaibhav/chess/PieceFactory.java
@@ -1,5 +1,7 @@
 package com.vaibhav.chess;
 
+import com.vaibhav.chess.piece.*;
+
 public class PieceFactory {
     private static final String BISHOP = "BISHOP";
     private static final String HORSE = "HORSE";
@@ -7,4 +9,34 @@ public class PieceFactory {
     private static final String PAWN = "PAWN";
     private static final String QUEEN = "QUEEN";
     private static final String ROOK = "ROOK";
+
+    public static Piece getPiece(String name, ICellDataManager dataManager) {
+        Piece piece = null;
+        if (name != null && name.length() > 0) {
+            switch (name) {
+                case KING:
+                    piece = new King();
+                    break;
+                case HORSE:
+                    piece = new Horse();
+                    break;
+                case QUEEN:
+                    piece = new Queen();
+                    break;
+                case BISHOP:
+                    piece = new Bishop();
+                    break;
+                case ROOK:
+                    piece = new Rook();
+                    break;
+                case PAWN:
+                    piece = new Pawn();
+                    break;
+                default:
+                    return null;
+            }
+            piece.setCellDataManager(dataManager);
+        }
+        return piece;
+    }
 }
